b3312b61ceaa24585ab0b4c56a00d8baa475ebb4
Nguyen Ngoc My <nguyenngocmy1311@gmail.com>
2025-04-06 10:37:26 +0700

    update code

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -7,14 +7,11 @@ const router = Router();
 router.post("/register", AuthController.register);
 router.get("/checkemail", AuthController.checkEmail);
 router.post("/checkemail", AuthController.checkEmail);
-
 router.post("/login", AuthController.login);
 router.post("/forgotpassword", AuthController.forgotpassword);
 router.put("/changepassword", AuthController.changepassword);
-
 router.post("/google-login", GoogleAuthMiddleware.verifyGoogleToken, AuthController.googleLogin);
 router.get("/google/callback", AuthController.googleCallback);
-
 router.post("/verifyotp", AuthController.verifyOtpEmail);
 router.post("/logout", AuthController.logout);
 
